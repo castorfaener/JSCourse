ece38b69977643484bd268344dd3916dc4a8e4e0
castorfaener <castorfaener@gmail.com>
2021-04-28 08:25:16 +0200

    Ejercicio3 Ok

--- Javascript/Hola-mundo/js/13-Ejercicio3.js
+++ Javascript/Hola-mundo/js/13-Ejercicio3.js
@@ -6,12 +6,39 @@
 
 var numero1 = 0;
 var numero2 = 0;
+var max;
+var min;
 
 do{
-	numero1 = parseInt(prompt("Introduce el primer número"));
+	numero1 = parseInt(prompt("Introduce el primer número"));					//Verificamos que el dato introducido sea un numero
 }while(isNaN(numero1))
 
 do{
-	numero1 = parseInt(prompt("Introduce el segundo número"));
+	numero2 = parseInt(prompt("Introduce el segundo número"));				//Verificamos que el dato introducido sea un numero
 }while(isNaN(numero2))
+
+if(numero1>numero2){
+	max=numero1;
+	min=numero2;
+
+
+	console.log("Los numeros comprendidos entre " + min + " y " + max + " son:"); 
+
+	for(var i=min+1;i<max;i++){
+		console.log(i);
+	}
+}else if(numero1<numero2){
+	max=numero2;
+	min=numero1;
+
+	console.log("Los numeros comprendidos entre " + min + " y " + max + " son:"); 
+
+	for(var i=min+1;i<max;i++){
+		console.log(i);
+	}
+
+}else{
+	alert("Los numeros introducidos son iguales");
+}
+
 
