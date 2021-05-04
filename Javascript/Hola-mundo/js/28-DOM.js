71f5da0dc2ae10fac041d6cfae90f28fa66a567c
castorfaener <castorfaener@gmail.com>
2021-05-04 13:48:54 +0100

    DOM2

--- Javascript/Hola-mundo/js/28-DOM.js
+++ Javascript/Hola-mundo/js/28-DOM.js
@@ -2,7 +2,38 @@
 
 // DOM - Document Object Model
 
+function cambiaColor(color){
+	caja.style.background = color;
+}
+
+//conseguir elementos con un ID concreto
+
 var caja=document.getElementById("micaja");
 console.log(caja);
 
 caja.innerHTML = "He modificado el archivo";
+caja.style.background = "red";
+caja.style.padding = "20px";
+caja.style.color = "white";
+
+
+//conseguir elementos por su etiqueta
+
+var todosLosDivs = document.getElementsByTagName("div");
+
+todosLosDivs[2].style.background="green"
+;
+
+console.log(todosLosDivs);
+
+
+
+
+
+//conseguir elementos por su clase
+
+
+
+
+
+
